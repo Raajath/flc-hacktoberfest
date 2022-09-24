1b14cc87e4a8fc6dd86796a4d17845a5b975f78a
Rajath .N <89772947+Raajath@users.noreply.github.com>
2022-09-24 15:54:31 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 export const Data =[
     {
-        name: "Nagaraj Pandith",
-        username: "nagarajpandith",
-        branch: "CSE",
+        name: "Rajath N",
+        username: "rajath",
+        branch: "ISE",
         year: "3rd",
     },
     
